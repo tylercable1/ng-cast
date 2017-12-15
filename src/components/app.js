876be2f45da7c5d9ae13ccf78472331600ec5dad
tylercable1 <tylercable7@gmail.com>
2017-12-15 15:41:57 -0800

    Render app in index.html

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,19 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    this.currentVideo = this.videos[0];
+    
+    this.selectVideo = () => {
+      
+    };
+    this.searchResults = () => {
+      
+    };
+  },
+
+
+
+  templateUrl: 'src/templates/app.html'
 });
