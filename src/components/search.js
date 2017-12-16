bdc70d983862990edf96696eef597bb0fe01d0b9
tylercable1 <tylercable7@gmail.com>
2017-12-16 12:34:45 -0800

    debouncing

--- src/components/search.js
+++ src/components/search.js
@@ -5,8 +5,17 @@ angular.module('video-player')
   },
   controller: function(youTube) {
     this.service = youTube;
+    this.liveSearch = function(query) {
+      this.search(query);
+    };
     this.search = (query) => {
       this.service.search(this.result, query);
+    };
+    
+    this.handleEnter = function(e, query) {
+      if (e === 13) {
+        this.search(query);
+      }
     };
   },
   
