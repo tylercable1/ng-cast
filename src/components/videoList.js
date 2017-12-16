e170aeaf8e468c1cbda141d9e2939d755ccba8bf
tylercable1 <tylercable7@gmail.com>
2017-12-15 17:29:04 -0800

    Render video list with accurate title and description and thumbnail

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,11 @@
 angular.module('video-player')
 .component('videoList', {
+  bindings: {
+    videos: '<'
+  },
+  controller: function() {
+
+  },
+  
   templateUrl: 'src/templates/videoList.html'
 });
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,12 @@
 angular.module('video-player')
 .component('videoListEntry', {
+  
+  bindings: {
+    video: '<'
+  },
+  
+  controller: function() {
+
+  },
   templateUrl: 'src/templates/videoListEntry.html'
 });
